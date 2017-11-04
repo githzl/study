ede8c4d1ea5b0b25f3833e03e49198a4e21f6d97
githzl <16955276@qq.com>
2017-11-04 22:54:34 +0800

    Update websocketserver.php

--- websocketserver.php
+++ websocketserver.php
@@ -26,38 +26,20 @@
 		for($i = 1;$i < count($info);$i++){
 		$server->push($info[$i],$obj->user."加入聊天室");
 		}
-		//var_dump($redis->smembers($request->cookie['group']));
-	/*foreach($redis->smembers('lixian') as $user){
-			if($request->cookie['username'] == $user){
-			foreach($redis->lrange('xiaoxi',0,-1) as $message){
-				$server->push($request->fd,$message);
-				}
-			}
-		}*/
-	//var_dump($redis->smembers($request->cookie['group']));
-	//	foreach($redis->smembers($request->cookie['group']) as $fd){
-	//		$server->push($fd,$obj->user."进入");
-	//	}
 	});
+
 	$server->on('message',function($server,$request){
 		global $redis;
 		$info = $redis->hgetall('hfd'.$request->fd);
 		$obj = json_decode(substr(base64_decode($info['username']),0,-3));
-	//var_dump($obj->user);
-	//var_dump($info['group']);
 	foreach($redis->smembers($info['group']) as $id){
 		if($id != 0){
 		$server->push($id,$obj->user."说".htmlspecialchars($request->data));
-		
-	}
+		}
 	}
 	$redis->rpush('xiaoxi'.$info['group'],$obj->user."说".$request->data);
-/*foreach($server->connections as $fd){
-		
-			$server->push($fd,$request->data);
-		}
-		$redis->rpush('xiaoxi',$request->data);*/
 	});
+
 	$server->on('close',function($server,$request){
 		global $redis;
 		$info = $redis->hgetall("hfd".$request);
@@ -69,17 +51,5 @@
 		}
 		$redis->sadd('lixian'.$info['group'],$obj->user);
 		$redis->srem($info['group'],$request);
-		//$obj = json_decode(substr(base64_decode($info['username']),0,-3));
-		//var_dump($redis->smembers($info['group']));
-	/*	
-	var_dump($info);
-		$obj = json_decode(substr(base64_decode($request->cookie['username']),0,-3));
-		foreach($server->connections as $fd){
-			if($fd != $request){
-			$server->push($fd,$redis->get($request)."离开");
-			}
-		}
-		$redis->sadd('lixian',$redis->get($request));
-	*/	
 	});
 	$server->start();
